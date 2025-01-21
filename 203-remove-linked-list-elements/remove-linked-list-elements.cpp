29dac3b21ad60aa854846d80075380ca5d725461
Bhavesh <142612199+Infinity2959N@users.noreply.github.com>
2025-01-21 15:42:08 +0530

    Time: 0 ms (100.00%) | Memory: 20.3 MB (13.51%) - LeetSync

---
+++ 203-remove-linked-list-elements/remove-linked-list-elements.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        // Create a dummy node and point its next to head
+        ListNode* dummy = new ListNode();  
+        dummy->next = head;
+        
+        // Initialize the tail pointer to the dummy node
+        ListNode* tail = dummy;
+        
+        
+        while (tail->next != nullptr) {
+            if (tail->next->val == val) {
+                // If the value matches, delete the node
+                ListNode* ptr = tail->next;
+                tail->next = ptr->next; 
+                delete ptr;  // Free the memory
+            } else {
+                tail = tail->next;
+            }
+        }
+        
+        // Return the updated list, starting from dummy's next node
+        head = dummy->next;
+        
+        // Cleanup the dummy node
+        delete dummy;
+        
+        return head;
+    }
+};
