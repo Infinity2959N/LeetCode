0481b094003ecc2385cea21943de63cae081daeb
Bhavesh <142612199+Infinity2959N@users.noreply.github.com>
2025-01-27 16:23:33 +0530

    Time: 15 ms (74.51%) | Memory: 25.6 MB (62.65%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> set;
+
+        for(const string& s: strs){
+            string key=s;
+            stable_sort(key.begin(),key.end());
+            set[key].push_back(s);
+        }
+
+        vector<vector<string>> result;
+        for(auto& entry : set){
+            result.push_back(entry.second);
+        }
+        return result;
+    }
+};
