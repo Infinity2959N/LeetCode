f8a59fc67477d6ec009ab46e3cf13a7a94364312
Bhavesh <142612199+Infinity2959N@users.noreply.github.com>
2025-02-26 21:06:05 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (94.20%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n= nums.size();
-        vector<int> leftArr(n);
-        vector<int> rightArr(n);
-        leftArr[0]=1;
-        rightArr[n-1]=1;
-        for(int i=1; i<n; i++){  //To store left product
-            leftArr[i]=leftArr[i-1]*nums[i-1];
+        int n = nums.size();
+        
+        // The result array
+        vector<int> result(n, 1);
+        
+        // Step 1: Calculate the prefix product (left product)
+        int leftProduct = 1;
+        for (int i = 0; i < n; ++i) {
+            result[i] = leftProduct;
+            leftProduct *= nums[i];
         }
-        for(int i=n-2; i>=0; i--){ //To store right product
-            rightArr[i]=rightArr[i+1]*nums[i+1];
+        
+        // Step 2: Calculate the suffix product (right product) and multiply it with the result array
+        int rightProduct = 1;
+        for (int i = n - 1; i >= 0; --i) {
+            result[i] *= rightProduct;
+            rightProduct *= nums[i];
         }
+        
+        return result;
 
-        for(int i=0; i<n; i++){  //To store left product
-            leftArr[i]*=rightArr[i];
-        }
-        return leftArr;
     }
 };
