338a5914b8f310e70c1d6e93439652183ece8935
Bhavesh <142612199+Infinity2959N@users.noreply.github.com>
2025-02-27 10:03:21 +0530

    Time: 1 ms (69.49%) | Memory: 22.7 MB (67.62%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,37 +1,19 @@
-class Solution {
+class Solution
+{
 public:
-    bool isValidSudoku(vector<vector<char>>& board) {
-        // Rows
-        for (int row = 0; row < 9; row++) {
-            unordered_set<char> seen;
-            for (int i = 0; i < 9; i++) {
-                if (board[row][i] == '.') continue; //Empty box
-                if (seen.count(board[row][i])) return false;    //If found already
-                seen.insert(board[row][i]);
-            }
-        }
-        // Columns
-        for (int col = 0; col < 9; col++) {
-            unordered_set<char> seen;
-            for (int i = 0; i < 9; i++) {
-                if (board[i][col] == '.') continue;
-                if (seen.count(board[i][col])) return false;
-                seen.insert(board[i][col]);
-            }
-        }
-        // Squares: (row/3, col/3)
-        for (int square = 0; square < 9; square++) {
-            unordered_set<char> seen;
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    int row = (square / 3) * 3 + i;
-                    int col = (square % 3) * 3 + j;
-                    if (board[row][col] == '.') continue;
-                    if (seen.count(board[row][col])) return false;
-                    seen.insert(board[row][col]);
+    bool isValidSudoku(vector<vector<char> > &board)
+    {
+        int used1[9][9] = {0}, used2[9][9] = {0}, used3[9][9] = {0};
+        
+        for(int i = 0; i < board.size(); ++ i)
+            for(int j = 0; j < board[i].size(); ++ j)
+                if(board[i][j] != '.')
+                {
+                    int num = board[i][j] - '0' - 1, k = i / 3 * 3 + j / 3;
+                    if(used1[i][num] || used2[j][num] || used3[k][num])
+                        return false;
+                    used1[i][num] = used2[j][num] = used3[k][num] = 1;
                 }
-            }
-        }
         
         return true;
     }
