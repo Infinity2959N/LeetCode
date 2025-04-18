fab58426082ff3428e6ea82e8364377fddb14a1c
Bhavesh <142612199+Infinity2959N@users.noreply.github.com>
2025-04-18 15:49:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (99.62%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
-    bool checkValidString(string s) {   //using greedy
-        int low=0,high=0;
-        /*
-        if (, increase both;
-        if ), decrese both;
-        if *, decrease low, increase high.
-
-        if high becomes -1 at an point, return false, if low becomes -1, reset
-        in the end, if low=0, return true.
-        */
-        for(char c: s){
-            if(c=='('){
+    bool checkValidString(string s) {
+        // Greedy approach: Low: min number of (, high: max num of (. (One set = remove one from both)
+        int low=0, high=0;
+        for(auto ch: s){
+            if(ch=='('){
                 low++;
                 high++;
-            }else if(c==')'){
+            }
+            else if(ch==')'){
                 low--;
                 high--;
-            }else{
+            }
+            else{
                 low--;
                 high++;
             }
-            if(high<0)   return false;
-            if(low<0)   low=0;
+            if(high < 0)    return false;   //too many ')'
+            low= max(0, low);
         }
-        return low==0;
-    }    
+        return (low== 0 || high == 0);
+    }
 };
