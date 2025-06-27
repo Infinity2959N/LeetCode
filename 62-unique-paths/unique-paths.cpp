d6aca2337115d31103a184327f61aee18b1fcc03
Bhavesh <142612199+Infinity2959N@users.noreply.github.com>
2025-06-27 15:44:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (79.68%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        //memoization
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        //bottom up
-        return solveDP(m-1, n-1, dp);
-    }
-private: 
-    int solveDP(int i, int j, vector<vector<int>>& dp){
-        if(i==0 || j==0)    return 1;
-        if(dp[i][j]!= -1)   return dp[i][j];
-        // dp[i][j]= going up+ going left
-        return dp[i][j]= solveDP(i-1, j, dp)+ solveDP(i, j-1, dp);
+        //tabu
+        vector<int> dp(n, 1);
+
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                dp[j]= dp[j]+ dp[j-1]; // Basically: dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[n-1];
     }
 };
