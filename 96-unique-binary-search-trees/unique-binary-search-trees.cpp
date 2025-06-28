753e54e020ac18086562b39ad72578f127ddf75b
Bhavesh <142612199+Infinity2959N@users.noreply.github.com>
2025-06-28 20:54:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (38.78%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int numTrees(int n) {
-        vector<int> dp(n+1, -1);
-        return solve(n, dp);
+        //vector<int> dp(n+1, -1);
+        //return solve(n, dp);
+        return solveTabu(n);
     }
 
 private:
@@ -14,5 +15,17 @@ private:
             ans+= solve(i-1, dp)* solve(n-i, dp);
         }
         return dp[n] =ans;
+    }
+
+    int solveTabu(int n){
+        vector<int>dp(n+1, 0);
+        dp[0]= dp[1]= 1;
+
+        for(int i=2; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                dp[i]+= dp[j-1]*dp[i-j];
+            }
+        }
+        return dp[n];
     }
 };
