a13f115dad1397566bdadca8c46f658b053e9285
Bhavesh <142612199+Infinity2959N@users.noreply.github.com>
2025-06-29 21:43:15 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (32.94%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        // The greedy approach is to try to jump as far as possible
-        int farthest=0, n= nums.size();
-
+        int farthest=0, n=nums.size();
         for(int i=0; i< n; i++){
-            int num= nums[i];
-            if(i > farthest) return false;  //cannot reach at i
-            farthest= max(num+i, farthest);
-            if(farthest >= n-1)   return true;
+            if(farthest >= n-1) return true;    //we can reach last index
+            if(farthest< i) return false;   // Got stuck. We are processing a node we cannot reach
+            int currFarthest= i+ nums[i];
+
+            farthest= max(farthest, currFarthest);
         }
-        return false;
+
+        return true;
     }
 };
