7145d26434aa7ccaa53285e385243a4d1f569985
Bhavesh <142612199+Infinity2959N@users.noreply.github.com>
2025-07-04 09:03:28 +0530

    Time: 55 ms (43.00%) | Memory: 28.5 MB (39.35%) - LeetSync

--- 741-cherry-pickup/cherry-pickup.cpp
+++ 741-cherry-pickup/cherry-pickup.cpp
@@ -1,45 +1,49 @@
 class Solution {
 public:
+    int n;
+    vector<vector<vector<int>>> memo;
+
+    int dfs(vector<vector<int>>& grid, int r1, int c1, int r2) {
+        int c2 = r1 + c1 - r2;
+
+        // Bounds check
+        if (r1 >= n || c1 >= n || r2 >= n || c2 >= n ||
+            grid[r1][c1] == -1 || grid[r2][c2] == -1)
+            return INT_MIN;
+
+        // Reached bottom-right
+        if (r1 == n - 1 && c1 == n - 1)
+            return grid[r1][c1];
+
+        if (memo[r1][c1][r2] != -1)
+            return memo[r1][c1][r2];
+
+        int cherries = 0;
+
+        // Same cell? Only one cherry
+        if (r1 == r2 && c1 == c2)
+            cherries += grid[r1][c1];
+        else
+            cherries += grid[r1][c1] + grid[r2][c2];
+
+        // Try all combinations of moves
+        int next = max({
+            dfs(grid, r1 + 1, c1, r2 + 1),  // down, down
+            dfs(grid, r1, c1 + 1, r2),      // right, right
+            dfs(grid, r1 + 1, c1, r2),      // down, right
+            dfs(grid, r1, c1 + 1, r2 + 1)   // right, down
+        });
+
+        if (next == INT_MIN)
+            return memo[r1][c1][r2] = INT_MIN;
+
+        return memo[r1][c1][r2] = cherries + next;
+    }
+
     int cherryPickup(vector<vector<int>>& grid) {
-        int n = grid.size();
-        // dp[k][i][j] -> max cherries collected when:
-        // Person A is at (i, k - i) and Person B is at (j, k - j)
-        vector<vector<vector<int>>> dp(2 * n - 1,
-            vector<vector<int>>(n, vector<int>(n, -1)));
+        n = grid.size();
+        memo = vector(n, vector(n, vector<int>(n, -1)));
 
-        dp[0][0][0] = grid[0][0];
-
-        for (int k = 1; k < 2 * n - 1; ++k) {
-            for (int i = max(0, k - (n - 1)); i <= min(n - 1, k); ++i) {
-                for (int j = max(0, k - (n - 1)); j <= min(n - 1, k); ++j) {
-                    int r1 = i, c1 = k - i;
-                    int r2 = j, c2 = k - j;
-
-                    if (grid[r1][c1] == -1 || grid[r2][c2] == -1)
-                        continue;
-
-                    int max_prev = -1;
-                    for (int pi = r1 - 1; pi <= r1; ++pi) {
-                        for (int pj = r2 - 1; pj <= r2; ++pj) {
-                            if (pi >= 0 && pj >= 0 &&
-                                dp[k - 1][pi][pj] != -1) {
-                                max_prev = max(max_prev, dp[k - 1][pi][pj]);
-                            }
-                        }
-                    }
-
-                    if (max_prev == -1)
-                        continue;
-
-                    int cherries = grid[r1][c1];
-                    if (r1 != r2 || c1 != c2)
-                        cherries += grid[r2][c2];
-
-                    dp[k][r1][r2] = max_prev + cherries;
-                }
-            }
-        }
-
-        return max(0, dp[2 * n - 2][n - 1][n - 1]);
+        return max(0, dfs(grid, 0, 0, 0)); // Starting from (0,0) for both
     }
 };
