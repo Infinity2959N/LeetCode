b759580a62e948b00552194ecffdc43ecdde60ed
Bhavesh <142612199+Infinity2959N@users.noreply.github.com>
2026-01-30 21:48:51 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (18.07%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> res;
-        int first=-1, second=-1;
+        // Approach: 2 binary searchs, one for leftmost and other for rightmost occurence
         int left= 0, right= nums.size()-1;
-        //First occurence
+        int lo= -1, ro=-1;     // leftoccurence and rightoccurene
+        // Left occurence
         while(left<=right){
-            int mid= left+ (right-left)/2;
-            if(nums[mid]==target){
-                first=mid;
-                right=mid-1;
-            }else if(nums[mid]<target){
-                left=mid+1;
+            int mid= left+ (right- left)/ 2;
+            if(nums[mid]> target){
+                right= mid-1;
+            }else if(nums[mid]==target){
+                lo=mid;
+                right= mid-1;
+            }else{
+                left= mid+1;
             }
-            else    right=mid-1;
         }
-        res.push_back(first);
-        left=0, right=nums.size()-1;
-        //last occurence
+        left= 0, right= nums.size()-1;
         while(left<=right){
-            int mid= left+ (right-left)/2;
-            if(nums[mid]==target){
-                second=mid;
-                left=mid+1;
-            }else if(nums[mid]<target){
-                left=mid+1;
+            int mid= left+ (right- left)/ 2;
+            if(nums[mid]> target){
+                right= mid-1;
+            }else if(nums[mid]==target){
+                ro=mid;
+                left= mid+1;
+            }else{
+                left= mid+1;
             }
-            else    right=mid-1;
         }
-        res.push_back(second);
-        return res;
+
+        return {lo, ro};
     }
 };
