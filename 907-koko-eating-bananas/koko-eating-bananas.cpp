a056ae103f3322fb866a750a0244fcd1255e3134
Bhavesh <142612199+Infinity2959N@users.noreply.github.com>
2026-02-01 21:06:16 +0530

    Time: 7 ms (71.42%) | Memory: 23 MB (44.81%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        //helper fuinction to calculate the time:
-        auto time=[&](int k){   //lambda function
-            long long hours=0;
-            for(int pile: piles){
-                hours+= (pile+ k-1)/k;  //returns the ceil of pile/k
-            }
-            return hours;
-        };
-        
-        int beg= 1, end= *max_element(piles.begin(), piles.end());
+        // n piles. ith pile has piles[i] bananas, guards wll come back in h hours
+        // banana eating speed is k. We have to return the minimum k that allows koko to eat all bananas within  hours.
 
-        while(beg<=end){
-            int mid= beg+ (end-beg)/2;
-            if(time(mid)<=h){
-                end=mid-1;  //try a slower speed
+        // Approach: Apply binary search on the range of k. If she can't eat all bananas within the current k, search in right of k, else store k as temporary answer and search on left of k.
+
+        // Selecting the range of k: Atleast 1 to atmost the size of the largest pile.
+        auto max_el= max_element(piles.begin(), piles.end());
+        int k= *max_el;
+
+        int low=1, high=k;
+        while(low<= high){
+            int mid= low+ (high-low)/ 2;
+            if(valid(mid, piles, h)){
+                k= mid;
+                high= mid-1;
             }else{
-                beg=mid+1;
+                low= mid+1;
             }
         }
-        return beg;
+        return k;
+    }
+private:
+    bool valid(int k, vector<int> &piles, int h){
+        // To check if Koko can eat all bananas in h hours at k rate
+        long long tot_hrs= 0;
+        for(int i: piles){
+            tot_hrs+= (i+k-1)/k;    // Essentially calculating ceil of [pile/k] that will find total hours koko will spend on i'th pile at k speed. The function is a mathematical "trick" to calculate the ceil
+        }
+        return tot_hrs<= h;
     }
 };
