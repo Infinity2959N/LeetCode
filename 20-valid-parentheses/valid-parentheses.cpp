048aa8d89c5d104dccf8b7da6ae095c0ca5e14cd
Bhavesh <142612199+Infinity2959N@users.noreply.github.com>
2026-02-04 21:26:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (66.78%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto it: s){
+            if(it=='(' || it=='{' || it=='['){
+                st.push(it);
+            }else if(it==')' || it=='}' || it==']'){
+                if(st.empty()) return false;
+
+                if((it==')' && st.top()!= '(')
+                || (it=='}' && st.top()!= '{')
+                || (it==']' && st.top()!= '[')){
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return (st.empty())? true: false;
+    }
+};
