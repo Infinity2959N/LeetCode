62302440a11d4496877c7e5d5c4e8bceb32401ca
Bhavesh <142612199+Infinity2959N@users.noreply.github.com>
2026-02-16 18:49:28 +0530

    Time: 0 ms (100.00%) | Memory: 65.2 MB (40.46%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        //use Floyd's tortoise and hare algorithm?
-        int tortoise=nums[0], hare=nums[0], i=0, size=nums.size();
-        
-        //1. Find intersection point inside the cycle
-        do{
-            tortoise=nums[tortoise];    //move 1 step
-            hare=nums[nums[hare]];  //move 2 steps
-        }
-        while(tortoise!=hare);
+        // Floyd's tortoise and hare
+        int slow = nums[0];
+        int fast = nums[0];
 
-        //2. Find the entrance to the cycle:
-        tortoise=nums[0];    //reset
-        while(tortoise!=hare){
-            tortoise=nums[tortoise]; //1 step
-            hare=nums[hare];    //1 step
-        }
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
 
-        return tortoise;
+        fast = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;  // or nums[slow]
     }
 };
