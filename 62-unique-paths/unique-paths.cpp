5dd8f8d599d4b1fe7d3f52b588191dcb6fe95db7
Bhavesh <142612199+Infinity2959N@users.noreply.github.com>
2026-02-16 19:41:59 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (92.83%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        //tabu
-        vector<int> dp(n, 1);
+        // Simple combinatorics question. 
+        // Use formula: (r+c)!/(r!*c!)
 
-        for(int i=1; i<m; i++){
-            for(int j=1; j<n; j++){
-                dp[j]= dp[j]+ dp[j-1]; // Basically: dp[i][j] = dp[i-1][j] + dp[i][j-1];
-            }
+        if(m == 1 || n == 1)
+            return 1;
+        m--;
+        n--;
+        if(m < n) {              // Swap, so that m is the bigger number
+            m = m + n;
+            n = m - n;
+            m = m - n;
         }
-        return dp[n-1];
+        long res = 1;
+        int j = 1;
+        for(int i = m+1; i <= m+n; i++, j++){       // Instead of taking factorial, keep on multiply & divide
+            res *= i;
+            res /= j;
+        }
+        return res;
     }
 };
