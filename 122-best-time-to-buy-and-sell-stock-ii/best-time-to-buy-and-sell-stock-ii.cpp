6401c37ab56228d4c124b8c486387d8e37524511
Bhavesh <142612199+Infinity2959N@users.noreply.github.com>
2026-03-02 20:14:35 +0530

    Time: 0 ms (100.00%) | Memory: 20 MB (52.60%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        // not required to buy at lowest and sell at highest, when you can just sell at higher price, buy at that price again and sell it again if needed.
+        // Aproach: Buy today and sell tomorrow if you can make profit, otherwise don't
+        if(prices.size()<=1)    return 0;
         int profit=0;
         for(int i=1; i<prices.size(); i++){
-            profit+= max(prices[i]-prices[i-1], 0);    
+            if(prices[i]> prices[i-1]){
+                profit+= prices[i]-prices[i-1];
+            }
         }
         return profit;
     }
