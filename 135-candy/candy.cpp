708d6a134c7fd34ef47d6670cf80840a1ea965f2
Bhavesh <142612199+Infinity2959N@users.noreply.github.com>
2026-03-10 12:54:08 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (53.64%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        // Two pass greedy. Difficult in name, but easy once approach clicks
-        int n=ratings.size();
-        vector<int> candies(n, 1);
+        // Requires O(n) complexity: Possible solutions: Either 2 pointer or two pass greedy (Greedy being the one that will work here)
+        // Double pass greedy:
+        // Pass 1: Ensure child has more candy than it's left neighbor if higher rating
+        // Pass 2: Ensure child has more candy than right neighbor if higher rating
+        int n= ratings.size();
+        vector<int> candies(n, 1); // Everyone starts with 1 candy
 
+        // Left to right
         for(int i=1; i<n; i++){
             if(ratings[i]> ratings[i-1])    candies[i]= candies[i-1]+1;
-            // Ensures increasing ratings get increasing candies compared to left neighbors
         }
 
-        // Right neighbors
+        // Right to left
         for(int i=n-2; i>=0; i--){
-            if(ratings[i]> ratings[i+1])    //cases: either already has more candies meaning no update, or not meaning update by candies[i+1]+1
-                candies[i]= max(candies[i], candies[i+1]+1);
+            if(ratings[i]> ratings[i+1])    candies[i]=max(candies[i], candies[i+1]+1);
+        }
+        int total=0;
+        for(int c: candies){
+            total+= c;
         }
 
-        //Return Total candies
-        return accumulate(candies.begin(), candies.end(), 0);
+        return total;
     }
 };
