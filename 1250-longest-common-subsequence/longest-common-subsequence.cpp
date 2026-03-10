2360b4e934439be0400d755a308a66644459c857
Bhavesh <142612199+Infinity2959N@users.noreply.github.com>
2026-03-10 14:51:21 +0530

    Time: 19 ms (87.71%) | Memory: 9.1 MB (93.86%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        int n= text1.size();
-        int m= text2.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
-
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
-                if(text1[i-1]== text2[j-1]){
-                    dp[i][j]= 1+ dp[i-1][j-1];
+        // Match: Diagonal top+1
+        // Not match: max(left, top)
+        // Memory optimization: two rows: curr and prev. Swap in each iteration
+        int m= text1.length(), n= text2.length();
+        vector<int> prev(n+1, 0);   // Have a filler as first word with 0 occurence
+        vector<int> curr(n+1, 0);
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n;j++){
+                if(text1[i-1]==text2[j-1]){
+                    curr[j]= prev[j-1]+1;
                 }else{
-                    dp[i][j]= max(dp[i][j-1], dp[i-1][j]);
+                    curr[j]= max(prev[j], curr[j-1]);
                 }
             }
+            prev= curr;
+            for(int i=0; i<=n; i++) curr[i]=0;
         }
-        return dp[n][m];
+        return  prev[n];
     }
 };
