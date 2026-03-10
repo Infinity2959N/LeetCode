90e337c40843b7882fb2103d5f99954af3a71eff
Bhavesh <142612199+Infinity2959N@users.noreply.github.com>
2026-03-10 11:34:12 +0530

    Time: 19 ms (58.40%) | Memory: 16.1 MB (42.16%) - LeetSync

--- 406-queue-reconstruction-by-height/queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/queue-reconstruction-by-height.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
-        // Approach: Step 1: sort based on heights (desc), if equal heights, sort based on k (asc)
-        sort(people.begin(), people.end(), [](const vector<int>& a, const vector<int>& b){
-            if(a[0]!= b[0]) return a[0] > b[0];
-            else return a[1] < b[1];
+        // h=height, k= how many people (taller or equal) are allowed in front of you
+        // Logic: Sort people by height descending, if same height, sort by k ascending
+        // Insert each in the result at the index equal to their k
+
+        sort(people.begin(), people.end(), [](const vector<int> &a, const vector<int> &b){
+            if(a[0]==b[0])
+                return a[1]<b[1];   // if height same: sort by k ascending
+            return a[0]> b[0];  // sort by height descending: Why? Taller people don't care about how many short people come in front of them
         });
 
-        // Why desc?: because we are inserting into a stack, meaning the previous value will slide to next index automatically
-
-        // Step 2: insert all elements into resultant vector in the kth index(Stack)
-        vector<vector<int>> result;
-        for(const vector<int>& person: people){
-            result.insert(result.begin()+ person[1], person);
+        // Step 2: Insert into vector at index k
+        vector<vector<int>> res;
+        for(const auto& p: people){
+            res.insert(res.begin()+p[1], p);    // Insert at k'th position
         }
-
-        return result;
-        
+        return res;
     }
 };
