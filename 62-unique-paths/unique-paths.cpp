adeb3af37562d961266b5b677385cefe983315ac
Bhavesh <142612199+Infinity2959N@users.noreply.github.com>
2026-03-10 14:33:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (85.23%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        // Simple combinatorics question. 
-        // Use formula: (r+c)!/(r!*c!)
+        // Using DP: Optimized for O(n) space
+        vector<int> row(n, 1);  // 1 way to move in every column in the first row
 
-        if(m == 1 || n == 1)
-            return 1;
-        m--;
-        n--;
-        if(m < n) {              // Swap, so that m is the bigger number
-            m = m + n;
-            n = m - n;
-            m = m - n;
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                row[j]= row[j]+row[j-1];    // paths[i,j]= paths[i-1,j]+paths[i,j-1]
+            }
         }
-        long res = 1;
-        int j = 1;
-        for(int i = m+1; i <= m+n; i++, j++){       // Instead of taking factorial, keep on multiply & divide
-            res *= i;
-            res /= j;
-        }
-        return res;
+        return row[n-1];
     }
 };
