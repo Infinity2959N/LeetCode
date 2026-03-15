e5bd36f6672ed6ec94ef69d6f9a255a888bcc5bb
Bhavesh <142612199+Infinity2959N@users.noreply.github.com>
2026-03-15 18:40:21 +0530

    Time: 12 ms (95.73%) | Memory: 18 MB (59.97%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount+1, INT_MAX);
-        dp[0]= 0;   // it takes 0 zero coins to amount to 0
-        for(int i=0; i<= amount; i++){
-            for(int coin: coins){
-                if (i - coin >= 0 && dp[i - coin] != INT_MAX)
-                    dp[i]= min(dp[i], 1+ dp[i-coin]);
+        sort(coins.begin(), coins.end());
+        int n= coins.size();
+        vector<int> dp(amount+1, amount+1);  // Starting from 0, to make any amount, there is no way, so we will need infinite coins. As number of coins to make a sum cannot be more than the sum, we can emulate infinity using "amount+1"
+        dp[0]= 0;   // 0 coins needed to make maount=0;
+
+        for(int i=1; i<= amount; i++){
+            for(int coin: coins){   //traverse till currAmount-1
+                int diff= i-coin;
+                if(diff< 0) break;
+
+                dp[i]= min(dp[i], 1+dp[diff]);  // read comment for this logic
             }
         }
-        return (dp[amount]==INT_MAX)? -1: dp[amount];
+        return (dp[amount]< amount+1)? dp[amount]: -1;  
     }
 };
