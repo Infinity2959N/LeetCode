bbde6eb14e3dc3e674ea8815e59865a19a72322f
Bhavesh <142612199+Infinity2959N@users.noreply.github.com>
2026-03-17 20:44:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (95.41%) - LeetSync

--- 1095-two-city-scheduling/two-city-scheduling.cpp
+++ 1095-two-city-scheduling/two-city-scheduling.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     int twoCitySchedCost(vector<vector<int>>& costs) {
-        int n= costs.size();    // We shall consider n instead of 2n for simplicity sake
-
+        // sort based on difference in costs, if cost of a is the cheapest, send to A, if it's most expensive, send to b. We don't look at what's cheaper, but rather at how much we save
         sort(costs.begin(), costs.end(), [](const vector<int>& a, const vector<int>& b){
             return (a[0]-a[1])< (b[0]-b[1]);
         });
-
-        int totalCost= 0;
-
-        for(int i=0; i<n/2; i++){   // First n/2 elements have cheaper a
-            totalCost+= costs[i][0];
+        // a[0]-a[1] is how much cheaper is going to city a than going to city b. THe lesser the better.
+        int cost=0;
+        for(int i=0; i<costs.size()/2; i++){
+           cost+= costs[i][0];
         }
-        for(int i=n/2; i<n; i++){   // Last n/2 elements have cheaper b
-            totalCost+= costs[i][1];
+        for(int i=costs.size()/2; i<costs.size(); i++){
+            cost+= costs[i][1];
         }
-
-        return totalCost;
+        return cost;
     }
 };
