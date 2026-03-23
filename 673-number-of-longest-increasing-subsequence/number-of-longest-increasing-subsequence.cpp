d11fe31b5822b639e1666505ff4cc3665bbd18b1
Bhavesh <142612199+Infinity2959N@users.noreply.github.com>
2026-03-23 19:38:37 +0530

    Time: 30 ms (58.18%) | Memory: 17.3 MB (89.52%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
     int findNumberOfLIS(vector<int>& nums) {
-        int n= nums.size();
-        vector<int> lis(n, 1);
-        vector<int> count(n, 1);
+        // We keep track of two things: Length and count of ways to get that length
+        // When i looks at j, there are 3 cases: 
+        // A: Bigger Sequence: dp[i]= dp[j]+1 and count[i]= count[j]
+        // B: Tie: (dp[j]+1== dp[i])    dp[i] stays same, but count[i]+= count[j]
+        // C: Smaller: Ignore
 
-        for(int i=0; i<n; i++){
+        // Todo: 1. Find the LIS 2. Sum all value in count, where for every i, dp[i]= max
+
+        int n= nums.size();
+        if(n==1)    return 1;
+        vector<int> dp(n, 1);
+        vector<int> count(n, 1);    // always 1 way to make an LIS of size 1 for each element
+
+        for(int i=1; i<n; i++){
             for(int j=0; j<i; j++){
-                if(nums[j]< nums[i]){
-                    if(lis[i] < lis[j]+1){
-                        lis[i]= lis[j]+1;
+                if(nums[i]> nums[j]){
+                    if(dp[j]+1> dp[i]){ // Case A
+                        dp[i]= dp[j]+1;
                         count[i]= count[j];
-                    }else if(lis[i] == lis[j]+1){
-                        count[i]+= count[j];
+                    }else if(dp[j]+1== dp[i]){
+                        count[i]+=count[j];
                     }
                 }
             }
         }
-        int maxlen= *max_element(lis.begin(), lis.end());
+        int maxElement= *max_element(dp.begin(), dp.end());
         int sum=0;
         for(int i=0; i<n; i++){
-            if(lis[i]==maxlen){
-                sum+= count[i];
-            }
+            if(dp[i]==maxElement)   sum+=count[i];
         }
         return sum;
     }
