f2301bd58e0b563c3c8ee22a071397ec51d985b1
Bhavesh <142612199+Infinity2959N@users.noreply.github.com>
2026-03-23 20:55:58 +0530

    Time: 3 ms (78.08%) | Memory: 14 MB (86.70%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        // Pattern: Non constant transition
-
+        // LIS using binary search: O(nlogn), over O(n^2) in traditional DP
+        // Patience Sorting (tails) logic
+        // We do not care about the sequence here
+        // We try to find smallest possible numbers to form a sequence of length l.
+        // Say given an array: 1,7,8,4,5,-1,9
+        // 1. Sequence: 1,7,8
+        // 2. Using Binary search find the optimal place for 4 and replace: 1,4,8
+            // First optimal place would be first element greater than current
+            // Use lower bound
+        // 3. Keep going: 1,4,5-> -1,4,5-> -1,4,5,9: max length: 4.
         int n= nums.size();
-        vector<int> dp(n, 1);   // Each makes a subsequene with itself
+        if(n==1)    return 1;
+
+        vector<int> temp;
+        temp.push_back(nums[0]);
+
+
         for(int i=1; i<n; i++){
-            for(int j=0; j<i; j++){
-                if(nums[j]< nums[i]){
-                    dp[i]= max(dp[i], dp[j]+1);
-                }
+            if(nums[i]> temp.back()){
+                temp.push_back(nums[i]);
+            }else{
+                int idx= lower_bound(temp.begin(), temp.end(), nums[i])- temp.begin();
+                temp[idx]= nums[i];
             }
         }
-        int maxi=1;
-        for(int n: dp){
-            maxi= max(maxi, n); 
-        }
-        return maxi;
+
+        return temp.size();
     }
 };
