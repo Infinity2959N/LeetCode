d756a028f9d46481e77b17e875e098cd4ff962c2
Bhavesh <142612199+Infinity2959N@users.noreply.github.com>
2026-04-07 16:25:29 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (87.12%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 1) return nums[0];
-        
-        // Two scenarios
-        int excludeFirst = robLinear(nums, 1, n - 1); // Rob from 1 to n-1
-        int excludeLast  = robLinear(nums, 0, n - 2); // Rob from 0 to n-2
+      // House robber variation
+      // Trick for circluar: if you start from 0, you won't  be able to rob last house: loop 1: 0->n-2
+      // If you want to rob last, go for first house: loop 2: 1->n-1
+      int n= nums.size();
+      if(n==1)  return nums[0];
 
-        return max(excludeFirst, excludeLast);
+      return max(robHouse(nums, 0, n-1), robHouse(nums, 1, n));
     }
-
 private:
-    int robLinear(const vector<int>& nums, int start, int end) {
-        int prev2 = 0, prev1 = 0;
-        for (int i = start; i <= end; ++i) {
-            int curr = max(nums[i] + prev2, prev1);
-            prev2 = prev1;
-            prev1 = curr;
+    int robHouse(const vector<int>& house, int s, int e){
+        // DP cases: for house i, either skip i and take best of i-1, or rob i and take best of i-2
+        int prev2= 0;
+        int prev1= 0;
+        for(int i=s; i<e; i++){
+            int curr= max((house[i]+ prev2), prev1);
+            prev2= prev1;
+            prev1= curr;
         }
         return prev1;
     }
