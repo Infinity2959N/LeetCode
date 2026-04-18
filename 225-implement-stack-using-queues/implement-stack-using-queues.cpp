9a8bfc9248296b6c528baa5a6b129b2ee8081319
Bhavesh <142612199+bhaveshpant321@users.noreply.github.com>
2026-04-18 18:02:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (15.32%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -1,30 +1,42 @@
 class MyStack {
 public:
-    queue<int> q1, q2;
-    MyStack() {}
+    /*
+    Stack: LIFO (Last in First out)
+    Queue: FIFO (First in First out)
+    If inserted: 1,2,3,4,5; And you pop; Stack: 5, Queue: 1
+
+    How to implement stack using queue:
+    We need 1 queue. 
+    Push: Add the element to the queue, now for n-1 times (where n is the size of queue) pop from back and push in front
+    Efficiency: O(n) for push, O(1) for pop
+    */
+    queue<int> st;
+    MyStack() {
+    }
     
     void push(int x) {
-        q2.push(x);
+        st.push(x);
+        int n= st.size();
 
-        while(!q1.empty()){
-            q2.push(q1.front());
-            q1.pop();
+        for(int i=0; i<n-1; i++){
+            int temp= st.front();
+            st.pop();
+            st.push(temp);
         }
-        swap(q1,q2);
     }
     
     int pop() {
-        int res=q1.front();
-        q1.pop();
-        return res;
+        int x= st.front();
+        st.pop();
+        return x;
     }
     
     int top() {
-        return q1.front();
+       return st.front(); 
     }
     
     bool empty() {
-        return q1.empty();
+       return st.empty(); 
     }
 };
 
