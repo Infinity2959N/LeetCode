eef3c645d89d99ce9cb727ac99d732de8954f501
Bhavesh <142612199+bhaveshpant321@users.noreply.github.com>
2026-04-23 19:26:33 +0530

    Time: 27 ms (55.67%) | Memory: 16.2 MB (88.63%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,50 +1,29 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int m= grid.size();
-        int n= grid[0].size();
-        int countIslands=0;
+        // 1 is land, 0 is water. Can't get to 0
+        // Approach: starting at 0,0; traverse the grid, updating the array upon each traversal, replace 1 with 2, say
+        // The number of times you encounter 1 in the graph upon iteration, is the number of islands
+        int count= 0, m= grid.size(), n= grid[0].size();
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(grid[i][j]=='1'){
-                    countIslands++;
-                    bfs(grid, i, j, m, n);
+                    count++;
+                    traverse(grid, i, j, m, n);
                 }
             }
         }
-        return countIslands;
+        return count;
     }
-private:
-    void DFS(vector<vector<char>>& grid, int i, int j, int m, int n){
-        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]=='0')   return;
-        
-        grid[i][j]='0';
+private: 
+    void traverse(vector<vector<char>>& grid, int i, int j, int m, int n){
+        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]!='1') return;
 
-        DFS(grid, i-1, j, m, n);
-        DFS(grid, i+1, j, m, n);
-        DFS(grid, i, j-1, m, n);
-        DFS(grid, i, j+1, m, n);
+        grid[i][j]='2';
+        // Can go up, left, right or down
+        traverse(grid, i-1, j, m, n);
+        traverse(grid, i, j-1, m, n);
+        traverse(grid, i+1, j, m, n);
+        traverse(grid, i, j+1, m, n);
     }
-
-    void bfs(vector<vector<char>>& grid, int i, int j, int m, int n) {
-    queue<pair<int, int>> q;
-    q.push({i, j});
-    grid[i][j] = '0';
-
-    vector<pair<int, int>> directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-
-    while (!q.empty()) {
-        auto [x, y] = q.front(); q.pop();
-
-        for (auto [dx, dy] : directions) {
-            int nx = x + dx, ny = y + dy;
-
-            if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == '1') {
-                grid[nx][ny] = '0';
-                q.push({nx, ny});
-            }
-        }
-    }
-}
-
 };
