4d44a78ac2ea19f011984f4d940dad8c995a55dd
Bhavesh <142612199+bhaveshpant321@users.noreply.github.com>
2026-04-23 22:24:05 +0530

    Time: 66 ms (49.19%) | Memory: 19.8 MB (84.49%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        int n= beginWord.size();
-        unordered_set<string> dict(wordList.begin(), wordList.end());
-        
-        queue<string> q;
-        q.push(beginWord);
+        // Think BFS
+        // Reach from beginword to endword, treat each character as a node in graph, move to next nodes.
+        // For each word in BFS queue, we change every index to all possible characters (a-z) and see if that new word exists in the map (to be initialized with wordlist prior), if yes we push that word on the stack
 
-        // Edge case: endWord not in the list:
-        if(!dict.count(endWord))    return 0;
+        unordered_set<string> words;
+        for(string word: wordList) words.insert(word);
+        if(!words.count(endWord)) return 0;
 
-        int count=1;    //count 1 as beginWord is the first word
-        while(!q.empty()){
-            int currSize= q.size();
-            while(currSize--){
-                string word= q.front();
-                if(word== endWord)  return count;
-                q.pop();
-                
-                for(int i=0; i<n; i++){
+        queue<string> bfs;
+        bfs.push(beginWord);
+        int steps=0;
+        while(!bfs.empty()){
+            steps++;
+            int s= bfs.size();
+            for(int x=0; x<s; x++){
+                string word= bfs.front(); bfs.pop();
+                for(int i=0; i<word.size(); i++){
+                    int curr=word[i]-'a';
                     string newWord= word;
-                    for(int letter= 0; letter< 26; letter++){
-                        if(newWord[i]!= 'a'+letter)    newWord[i]= 'a'+letter;
-                        if(dict.count(newWord)){
-                            q.push(newWord);
-                            dict.erase(newWord);
+                    for(int j=0; j<26; j++){
+                        if(j==curr) continue;
+                        newWord[i]= (char)j+'a';
+                        if(newWord==endWord) return steps+1;
+                        if(words.count(newWord)){
+                            bfs.push(newWord);
+                            words.erase(newWord);
                         }
                     }
                 }
             }
-            count++;    // Increment step after one full level
         }
-        return 0;
+        return 0; 
     }
 };
