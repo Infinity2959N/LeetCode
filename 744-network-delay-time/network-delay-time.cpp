9b94159dee102e9a4daa2c531ebb73a0b5bba126
Bhavesh <142612199+bhaveshpant321@users.noreply.github.com>
2026-04-23 21:52:32 +0530

    Time: 77 ms (80.13%) | Memory: 44.3 MB (81.63%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,45 +1,43 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        // Build the adjList
-        vector<vector<pair<int, int>>> graph(n+1);  // n+1 because in this problem, numbering is 1 based
-        for(auto& edge: times){
-            int u= edge[0], v=edge[1], time= edge[2];
-            graph[u].push_back({v, time});
+        // To start with times[k], which is essentially the source
+        // To find the minimum time it takes to reach the furthest node
+        // Dijkstra: We use a priority queue
+        // Initially, distance of each element from u will be infinity, update the distance 
+        // 1. Build the Adjacency List (Source -> {Neighbor, Weight})
+        
+        vector<vector<pair<int, int>>> adj(n + 1); // n+1 because nodes are 1-indexed
+        for (auto& edge : times) {
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+            adj[u].push_back({v, w});
         }
-        //Build the minHeap
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;  // Format: (time, node)
-        pq.push({0, k});    
+        
+        vector<int> dist(n+1, 1e9);
+        dist[0]= 0; // 1 indexed array, so 0 will never be accessed
+        dist[k]= 0;
 
-        // Build distance array
-        vector<int> dist(n+1, INT_MAX);
-        dist[k]=0;
+        // Maintain a priority queue (min heap) with known distances
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        pq.push({0, k});    // distance, node
 
-        // Build visited array: optimization
-        vector<bool> visited(n + 1, false);
-
-        // Standard Dijkstra
         while(!pq.empty()){
-            auto [time, node]= pq.top();
+            auto [dis, u]= pq.top();
             pq.pop();
-
-            if (visited[node]) continue;
-            visited[node] = true;
-
-            for(auto&[neighbor, weight]: graph[node]){
-                if(dist[neighbor]> time+weight){
-                    dist[neighbor]= time+weight;
-                    pq.push({dist[neighbor], neighbor});
+            if(dis> dist[u]) continue;   // if distance is greater than previous min distance, discard
+            // Syntax to access neighbors:
+            for(auto& [v, weight] : adj[u]) { 
+                if(dist[u] + weight < dist[v]) {
+                    dist[v] = dist[u] + weight;
+                    pq.push({dist[v], v});
                 }
             }
         }
 
-        // Find the max time: Question says minimum time, but minimum time for all nodes to recieve the signal would be the longest time taken for signal to reach from k node to any other node
-        int maxTime= 0;
-        for(int i=1; i<=n; i++){
-            if(dist[i]== INT_MAX)   return -1;
-            maxTime= max(maxTime, dist[i]);
-        }
-        return maxTime;
+        int delay= *max_element(dist.begin(), dist.end());
+
+        return delay==1e9? -1: delay;
     }
 };
